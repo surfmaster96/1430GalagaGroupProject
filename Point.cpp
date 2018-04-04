62e813b752f0a4ed8ab050002d5425344eadce1c
Andrew Zimmerman <andyszimmerman@gmail.com>
2018-04-04 10:33:21 -0600

    Updated Color and Point - Andrew Zimmerman
    Andrew Zimmerman updated color and point.

---
+++ Point.cpp
@@ -0,0 +1,25 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  Author: Martyn Staalsen
+//  Assignment Title: Group Project
+//  Assignment Description: Functions as a clone of Galaga.
+//  Due Date: 4/26/2018
+//  Date Created: 4/3/2018
+//  Date Last Modified: 4/4/2018
+//  Contributors: Andrew Zimmerman
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#include "Point.h"
+
+// ## Begin Constructors ##
+
+Point::Point()
+{
+    X = 0;
+    Y = 0;
+}
+
+Point::Point(int x, int y)
+{
+    X = x;
+    Y = y;
+}
