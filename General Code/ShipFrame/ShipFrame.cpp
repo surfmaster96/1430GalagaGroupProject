fd5b8ee1fc808fdbff458d446c7ad07cfa7126ac
Jiahaosakana <38049614+Jiahaosakana@users.noreply.github.com>
2018-04-12 23:56:40 -0500

    Add files via upload

--- General Code/ShipFrame/ShipFrame.cpp
+++ General Code/ShipFrame/ShipFrame.cpp
@@ -13,14 +13,37 @@
 // ## Begin Constructor ##
 ShipFrame::ShipFrame()
 {
-    for(int i = 0; i <= 2500;i++)
+    count = 2500;
+    for(int i = 0; i <= count;i++)
     {
         location[i].X = location[i].Y = 200;
         color[i].R = 0;
         color[i].B = 255;
         color[i].G = 255;
     }
+    
 }
+
+//## count ##
+
+void ShipFrame::setCount(int c){
+    count = c;
+}
+
+
+int ShipFrame::getCount() const{
+    return count;
+}
+
+void ShipFrame::setCenter(Point c){
+    center = c;
+}
+
+Point ShipFrame::getCenter() const{
+    return center;
+}
+
+
 
 // ## Begin Color ##
 void ShipFrame::setColor(int x, Color c)
@@ -45,20 +68,48 @@ Point ShipFrame::getLocation(int x) const
 }
 
 // ## Begin Display / Erase ##
-void ShipFrame::displayFrame(int x ,int move, ShipFrame myFrame, SDL_Plotter& g, double& X, double& Y)
+void ShipFrame::displayFrame(int moveSpeed, SDL_Plotter& g)
 {
     double m, n;
-    
+    int x = count;
+    Point C;
     for(int i = 0; i < x; i++)
     {
         m = location[i].X;
-        m += move;
+        m += moveSpeed;
         n = location[i].Y;
         // Update X and Y for bullet's new location.
-        X = location[x/3].X;
-        X += move;
-        Y = location[x/3].Y;
+        if(i == (x-5)){
+            C.X = location[x/3].X;
+            C.X += moveSpeed;
+            C.Y = location[x/3].Y;
+        }
+        setCenter(C);
         g.plotPixel(m, n, color[i].R, color[i].G, color[i].B);
+    }
+}
+
+void ShipFrame::displayBugs(double moveX, double moveY, SDL_Plotter& g, bool& fall)
+{
+    if(!fall){
+        Point C;
+        double m, n;
+        for(int i = 0; i < count; i++)
+        {
+            m = location[i].X;
+            m += moveX;
+            n = location[i].Y;
+            n += moveY;
+            
+            if(i == 100){
+                
+                C.X = m;
+                C.Y = n;
+                setCenter(C);
+            }
+            // Update X and Y for bullet's new location.
+            g.plotPixel(m, n, color[i].R, color[i].G, color[i].B);
+        }
     }
 }
 
@@ -75,3 +126,14 @@ void ShipFrame::eraseFrame(int x, int move, ShipFrame myFrame, SDL_Plotter& g)
     }
 }
 
+
+//*********************************** For firing
+void ShipFrame::displayFire(SDL_Plotter& g, Point start){
+    double m, n;
+    for(int i = 0; i < count; i++)
+    {
+        m = start.X + location[i].X;
+        n = start.Y + location[i].Y;
+        g.plotPixel(m, n, color[i].R, color[i].G, color[i].B);
+    }
+}
